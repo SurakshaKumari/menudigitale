b803ade0dd88aaefdd7f731779d4e25e2c7bd142
Suraksha <surakshak571@gmail.com>
2025-12-19 23:21:07 +0500

    add

--- frontend/src/utils/axios.ts
+++ frontend/src/utils/axios.ts
@@ -2,7 +2,12 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  // baseURL: 'http://localhost:5000/api',
+  baseURL:
+    process.env.REACT_APP_API_URL ||
+    process.env.VITE_API_URL ||
+    process.env.NEXT_PUBLIC_API_URL ||
+    'http://localhost:5000/api',
   headers: {
     'Content-Type': 'application/json'
   }
